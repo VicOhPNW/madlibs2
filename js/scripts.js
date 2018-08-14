99e1c2f7487d9430cd33ea0d75c526eab85700e0
David Mortkowitz and Victoria Oh <david@>
2018-08-14 11:42:05 -0700

    Add second For loop in Javascript, can choose between For or ForEach loop to operate form.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
     var madLibs = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
-    madLibs.forEach(function(madLib){
-      var userInput = $("input#" + madLib).val();
-      $("." + madLib).text(userInput);
-      });
+
+      for (var index = 0; index < madLibs.length; index ++){
+        var userInput = $("input#" + madLibs[index]).val();
+        $("." + madLibs[index]).text(userInput);
+      }
+
+    // madLibs.forEach(function(madLib){
+    //   var userInput = $("input#" + madLib).val();
+    //   $("." + madLib).text(userInput);
+    //   });
 
       $("#story").show();
       event.preventDefault();
